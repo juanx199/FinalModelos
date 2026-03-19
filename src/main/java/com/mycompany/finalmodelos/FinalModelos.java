3599833f5797147c509d1514bb2260b4032d246f
juanx199 <jccarvajalcamargo@gmail.com>
2026-03-18 19:55:25 -0500

    Estructura base del proyecto.

---
+++ src/main/java/com/mycompany/finalmodelos/FinalModelos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalmodelos;
+
+/**
+ *
+ * @author JUANCA
+ */
+public class FinalModelos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
